5e24cbe510fea6b6415cbb9612257d63acd0a797
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-05-01 02:09:37 -0300

    primerCommit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+import indexRouter from './routes/routes';
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(indexRouter)
+// app.use(express.static(join(__dirname, 'public')))
+
+app.listen(5050)
+console.log('Listening on PORT 5050');
